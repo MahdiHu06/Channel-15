35aaf6c42b54914a3b027dd35a886721536e4c2c
Mahdi El husseini <melhusse@ecelnx53.ecn.purdue.edu>
2025-11-04 21:18:19 -0500

    check line 93

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 #include "hardware/uart.h"
+#include "pico/binary_info.h"
 ////////////////////////////////////////////////////////////////////////
 
 // functions
@@ -89,7 +90,7 @@ void init_i2c() {
     gpio_pull_up(i2c_SCL);
 
     // Make the I2C pins available to picotool.... this is on the datasheet
-    bi_decl(bi_2pins_with_func(i2c_SDA, i2c_SCL, GPIO_FUNC_I2C));
+    // bi_decl(bi_2pins_with_func(i2c_SDA, i2c_SCL, GPIO_FUNC_I2C));
 }
 
 
