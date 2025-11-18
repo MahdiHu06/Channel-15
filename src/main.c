39d6f04994b32287033c092d8c5c23a6362b7bdd
Mahdi El Husseini <135082877+MahdiHu06@users.noreply.github.com>
2025-11-18 16:18:36 -0500

    Refactor UART and I2C initialization and BMP280 functions
    Updated I2C pin numbers and modified UART initialization. Adjusted I2C speed and refactored BMP280 read/write functions.

--- src/main.c
+++ src/main.c
@@ -44,38 +44,118 @@
 void init_i2c();
 
 // Pin numbers for I2C
-uint i2c_SDA = 26;
-uint i2c_SCL = 27;
+uint i2c_SDA = 30;
+uint i2c_SCL = 31;
 i2c_inst_t *i2c = i2c1;
 
+#define BUFSIZE 32
+char serbuf[BUFSIZE];
+int seridx = 0;
+int newline_seen = 0;
+
+// add this here so that compiler does not complain about implicit function
+void uart_rx_handler();
+void init_uart() { 
+    uart_init(uart0, 115200); 
+    gpio_set_function(1, UART_FUNCSEL_NUM(uart0, 1));
+    gpio_set_function(0, UART_FUNCSEL_NUM(uart0, 0));
+}
+void init_uart_irq() {
+    // disable FIFO
+    uart_set_fifo_enabled(uart0, false);
+
+    // mask the interrupt
+    uart0_hw->imsc = (1 << 4); 
+    irq_set_exclusive_handler(UART0_IRQ, uart_rx_handler);
+    
+    irq_set_enabled(UART0_IRQ, true);
+}
+
+void uart_rx_handler() {
+    // Acknowledge Interrupt
+    uart0_hw->icr = 1 << 4;
+
+    if (seridx == BUFSIZE) { return; }
+    
+    char c = uart0_hw->dr;      // get character
+
+    // check for what the character is
+    if (c == 0x0A) { newline_seen = 1; }
+    if ((c == 0x8) && (seridx > 0)) {
+        putchar(c);     // backspace
+        putchar(0x20);  // space
+        putchar(c);     // backspace
+        seridx--;   
+        serbuf[seridx] = '\0';
+        return;
+    }
+
+    // Otherwise
+    serbuf[seridx] = c;
+    uart_write_blocking(uart0, &serbuf[seridx], 1);
+    seridx++;
+}
+
+int _read(__unused int handle, char *buffer, int length) {
+    while (newline_seen == 0) { sleep_ms(5); }
+    newline_seen = 0;
+    for (int i = 0; i < seridx; i++) {
+        if (i == length) { break; }
+        buffer[i] = serbuf[i];
+    }
+    uint idx = seridx;
+    seridx = 0;
+    return idx;
+}
+
+int _write(__unused int handle, char *buffer, int length) {
+    for (int i = 0; i < length; i++) { uart_write_blocking(uart0, &buffer[i], 1); }
+    return length;
+}
 
 /* Initializing I2C */
 void init_i2c() {
-    i2c_init(i2c, 115200);
+    i2c_init(i2c, 100 * 1000);
     gpio_set_function(i2c_SDA, GPIO_FUNC_I2C);
     gpio_set_function(i2c_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(i2c_SDA);
     gpio_pull_up(i2c_SCL);
 }
+void bmp_write_reg(uint8_t addr, uint8_t reg, uint8_t value) {
+    uint8_t buffer[2] = {reg, value};
+    i2c_write_blocking(i2c, addr, buffer, 2, false);
+}
 
+void bmp_read_bytes(uint8_t addr, uint8_t reg, uint8_t *buffer, size_t length) {
+    i2c_write_blocking(i2c, addr, &reg, 1, true);
+    i2c_read_blocking(i2c, addr, buffer, length, false);
+}
 int main() {
-    init_i2c();
-    
-    // send to the BMP280 (let's try temp)
-    // slave address with RW == 0
-    uint8_t slave_addr = BMP280_ADDRESS_ALT * 2;     // * 2 because RW is '0'
-    uint8_t regAddr = BMP280_REGISTER_TEMPDATA;
-    uint8_t regData = 0;
-    uint8_t write_data[3] = {slave_addr, regAddr, regData};
-    // reading
-    uint8_t data[3] = {};
+    init_uart();
+    init_uart_irq();
 
-    for (;;) {    
-        i2c_write_blocking(i2c, slave_addr, write_data, 3, true);
-        slave_addr++;   // 0x76 to 0x77
-        i2c_read_blocking(i2c, slave_addr, data, 3, true);  
+    setbuf(stdout, NULL); // disable buffering for stdout
+    // printf("\nHello World!\n");
+    init_i2c();
+
+
+    uint8_t bmp_addr = BMP280_ADDRESS_ALT;
+    sleep_ms(500);
+
+    uint8_t chip_id = 0x0;
+    bmp_read_bytes(bmp_addr, BMP280_REGISTER_CHIPID, &chip_id, 1);
+    printf("Chip ID: 0x%X (expected 0x%X)\n", chip_id, BMP280_CHIPID);
+    
+
+    bmp_write_reg(bmp_addr, BMP280_REGISTER_CONTROL, 0x27);
+    for (;;) {        
+        // replace new line with null
+        uint8_t data[3];
+        bmp_read_bytes(bmp_addr, BMP280_REGISTER_TEMPDATA, data, 3);
+        int32_t temp_data = ((int32_t)data[0] << 12) | ((int32_t)data[1] << 4) | ((int32_t)data[2] >> 4);
+        printf("Temp: 0x%X\n", temp_data);
         
-        for (int i = 0; i < 4; i++) { printf("%d\n", data[i]); }
-    }
-    return EXIT_SUCCESS;    // should never hit
+        sleep_ms(1000);
+   }
+   return EXIT_SUCCESS;    // should never hit
 }
