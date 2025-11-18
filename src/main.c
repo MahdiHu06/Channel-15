5d1f3ac029f9275d7b335286048355a383e3aaac
Mahdi El husseini <melhusse@ecelnx53.ecn.purdue.edu>
2025-11-18 14:05:13 -0500

    Working on setting up i2c for BMP280

--- src/main.c
+++ src/main.c
@@ -8,78 +8,46 @@
 #include "hardware/i2c.h"
 #include "hardware/uart.h"
 #include "pico/binary_info.h"
+
 ////////////////////////////////////////////////////////////////////////
 
-// functions
-void init_i2c();
-void init_uart();
-void init_uart_irq();
-void uart_rx_handler();
+/* Definitions */
+#define BMP280_ADDRESS (0x77) /**< The default I2C address for the sensor. */
+#define BMP280_ADDRESS_ALT                                                     \
+  (0x76)                     /**< Alternative I2C address for the sensor. */
+#define BMP280_CHIPID (0x58) /**< Default chip ID. */
 
-// Definitions and global variables
-#define BUFSIZE 32
-char serbuf[BUFSIZE];
-int seridx = 0;
-int newline_seen = 0;
+/* Available Registers */
+#define BMP280_REGISTER_DIG_T1 0x88
+#define BMP280_REGISTER_DIG_T2 0x8A
+#define BMP280_REGISTER_DIG_T3 0x8C
+#define BMP280_REGISTER_DIG_P1 0x8E
+#define BMP280_REGISTER_DIG_P2 0x90
+#define BMP280_REGISTER_DIG_P3 0x92
+#define BMP280_REGISTER_DIG_P4 0x94
+#define BMP280_REGISTER_DIG_P5 0x96
+#define BMP280_REGISTER_DIG_P6 0x98
+#define BMP280_REGISTER_DIG_P7 0x9A
+#define BMP280_REGISTER_DIG_P8 0x9C
+#define BMP280_REGISTER_DIG_P9 0x9E
+#define BMP280_REGISTER_CHIPID 0xD0
+#define BMP280_REGISTER_VERSION 0xD1
+#define BMP280_REGISTER_SOFTRESET 0xE0
+#define BMP280_REGISTER_CAL26 0xE1 /**< R calibration = 0xE1-0xF0 */
+#define BMP280_REGISTER_STATUS 0xF3
+#define BMP280_REGISTER_CONTROL 0xF4
+#define BMP280_REGISTER_CONFIG 0xF5
+#define BMP280_REGISTER_PRESSUREDATA 0xF7
+#define BMP280_REGISTER_TEMPDATA 0xFA
+
+// Functions
+void init_i2c();
 
 // Pin numbers for I2C
 uint i2c_SDA = 26;
 uint i2c_SCL = 27;
 i2c_inst_t *i2c = i2c1;
 
-/* Uart Initilization */
-void init_uart(){
-    uart_init(uart0, 115200);
-    gpio_set_function(1, UART_FUNCSEL_NUM(uart0, 1));
-    gpio_set_function(0, UART_FUNCSEL_NUM(uart0, 0));
-}
-
-void init_uart_irq() {
-    // disable FIFO
-    uart_set_fifo_enabled(uart0, false);
-    uart0_hw->imsc = (1 << 4); 
-    irq_set_exclusive_handler(UART0_IRQ, uart_rx_handler);
-    irq_set_enabled(UART0_IRQ, true);   // Set to NVIC
-}
-
-void uart_rx_handler() {
-    uart0_hw->icr = 1 << 4; // Acknowledge Interrupt
-    // Don't exceed buffer size
-    if (seridx == BUFSIZE) { return; }
-
-    // check for what the character is
-    char c = uart0_hw->dr;      // get character
-    if (c == 0x0A) { newline_seen = 1; }
-    if ((c == 0x8) && (seridx > 0)) {
-        putchar(c);     // backspace
-        putchar(0x20);  // space
-        putchar(c);     // backspace
-        seridx--;   
-        serbuf[seridx] = '\0';
-        return;
-    }
-    // Otherwise
-    serbuf[seridx] = c;
-    uart_write_blocking(uart0, &serbuf[seridx], 1);
-    seridx++;
-}
-
-int _read(__unused int handle, char *buffer, int length) {
-    while (newline_seen == 0) { sleep_ms(5); }
-    newline_seen = 0;
-    for (int i = 0; i < seridx; i++) {
-        if (i == length) { break; }
-        buffer[i] = serbuf[i];
-    }
-    uint idx = seridx;
-    seridx = 0;
-    return idx;
-}
-
-int _write(__unused int handle, char *buffer, int length) {
-    for (int i = 0; i < length; i++) { uart_write_blocking(uart0, &buffer[i], 1); }
-    return length;
-}
 
 /* Initializing I2C */
 void init_i2c() {
@@ -88,98 +56,26 @@ void init_i2c() {
     gpio_set_function(i2c_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(i2c_SDA);
     gpio_pull_up(i2c_SCL);
-
-    // Make the I2C pins available to picotool.... this is on the datasheet
-    // bi_decl(bi_2pins_with_func(i2c_SDA, i2c_SCL, GPIO_FUNC_I2C));
 }
 
-
-/* Copied over from lab 7 */
-void cmd_gpio(int argc, char **argv) {
-    if (argc < 2) {
-        printf("argc needs to be at least 2.\n");    // I'm unsure of what this line should be
-        return;
-    }
-
-    if (strcmp(argv[1], "out") == 0) {
-        if (argc != 3) {
-            printf("argc needs to be equal to 3.\n");
-            return;
-        }
-        int pinNum = atoi(argv[2]);
-        if ((pinNum < 0) || pinNum > 47) {
-            printf("Invalid pin number: %d. Must be between 0 and 47.\n", pinNum);
-            return;
-        }
-        gpio_init(pinNum);
-        gpio_set_dir(pinNum, true);
-        printf("Initialized pin %d as an output.\n", pinNum);
-        return;
-    }
-    if (strcmp(argv[1], "set") == 0) {
-        if (argc != 4) {
-            printf("argc needs to be equal to 4.\n");
-            return;
-        }
-        int pinNum2 = atoi(argv[2]);
-        if ((pinNum2 < 0) || (pinNum2 > 47)) {
-            printf("Invalid pin number: %d. Must be between 0 and 47.\n", pinNum2);
-            return;
-        }
-        if ((gpio_get_dir(pinNum2) == 0)) {
-            return;
-        }
-        int pinValue = atoi(argv[3]);
-        if ((pinValue != 0) && (pinValue != 1)) {
-            printf("Error. Invalid argument.\n");
-            return;
-        }
-        gpio_put(pinNum2, pinValue);
-        printf("Set pin %d to %d\n", pinNum2, pinValue);
-        return;
-    }
-    printf("Unknown command: \n");
-}
-
-
-/* main */
 int main() {
-    init_uart();
-    init_uart_irq();
+    init_i2c();
+    
+    // send to the BMP280 (let's try temp)
+    // slave address with RW == 0
+    uint8_t slave_addr = BMP280_ADDRESS_ALT * 2;     // * 2 because RW is '0'
+    uint8_t regAddr = BMP280_REGISTER_TEMPDATA;
+    uint8_t regData = 0;
+    uint8_t write_data[3] = {slave_addr, regAddr, regData};
+    // reading
+    uint8_t data[3] = {};
 
-    setbuf(stdout, NULL); // Disable buffering for stdout
-
-    // Welcome message
-    printf("Channel-15's Peripheral Command Shell (PCS)\n");
-    printf("Enter a command below.\n\n");
-
-    // Inputs
-    int argc;
-    char *argv[10];
-    char input[100];
-
-    for (;;) {
-        printf("\r\n>");
-        fgets(input, sizeof(input), stdin);
+    for (;;) {    
+        i2c_write_blocking(i2c, slave_addr, write_data, 3, true);
+        slave_addr++;   // 0x76 to 0x77
+        i2c_read_blocking(i2c, slave_addr, data, 3, true);  
         
-        // This is needed
-        fflush(stdin);
-        
-        // replace new line with null
-        size_t j = strcspn(input, '\n');
-        input[j] = '\0';    // look into this
-        
-        // tokenize input string
-        char *token = strtok(input, " ");
-        for (argc = 0; argc < 10; argc++) { 
-            if (token == NULL) { break; }
-            argv[argc] = token;
-            token = strtok(NULL, " ");
-        }
-
-        // handle command
-        cmd_gpio(argc, argv);
+        for (int i = 0; i < 4; i++) { printf("%d\n", data[i]); }
     }
-
     return EXIT_SUCCESS;    // should never hit
 }
