983f933f32502888beb8b326d5ea10b8e1d3102b
Quincy D <>
2025-12-02 15:26:30 -0500

    Outlined main loop

---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+
+int main() {
+    // Init Pins for Radio
+    // Config Radio
+
+    // Init Pins for Sensor
+    // Config Sensor
+
+    // Start Radio Receive
+
+    // Begin Main Loop
+    // Wait for Request from Radio
+    // Send Acknowledgment
+
+    // Request Router Based on Measurement Type and Unit
+    // Get Measurement from Sensor
+
+    // Switch Radio to Transmit
+    // Send Measurement
+
+    // Switch Radio back to Receive
+    // Repeat Loop
+    return 0;
+}
